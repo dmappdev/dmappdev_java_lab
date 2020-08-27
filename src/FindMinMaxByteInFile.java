44ab41727d4e8116512a1226be9902d2f2e0eb75
dmappdev <dmappdev@outlook.com>
2020-08-27 12:47:10 -0400

    commit 2

---
+++ src/FindMinMaxByteInFile.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class FindMinMaxByteInFile {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = bufferedReader.readLine();
+        bufferedReader.close();
+        FileInputStream inputStream = new FileInputStream(fileName);
+        int min = Byte.MAX_VALUE;
+        while (inputStream.available() > 0) {
+            int inputByte = inputStream.read();
+            if (min > inputByte) min = inputByte;
+        }
+        inputStream.close();
+        System.out.println(min);
+        BufferedReader bufferedReader2 = new BufferedReader(new InputStreamReader(System.in));
+        String fileName2 = bufferedReader2.readLine();
+        bufferedReader2.close();
+        FileInputStream inputStream2 = new FileInputStream(fileName2);
+        int max = Integer.MIN_VALUE;
+        while (inputStream2.available() > 0){
+            int inputByte = inputStream2.read();
+            if (max < inputByte) max = inputByte;
+        }
+        inputStream2.close();
+        System.out.println(max);
+
+    }
+
+}
---
+++ src/StreamInOutTest.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class StreamInOutTest {
+
+    public static void main(String[] args) throws IOException {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        PrintStream console = new PrintStream(System.out);
+        console.println("Hello");
+
+        FileInputStream inputStream = new FileInputStream("c:/data.txt");
+        //Create a stream to write bytes to a file
+        FileOutputStream outputStream = new FileOutputStream("c:/result.txt");
+
+        while (inputStream.available() > 0) //as long as there are unread bytes
+        {
+            int data = inputStream.read(); // Read the next byte into the data variable
+            outputStream.write(data); // and write it to the second stream
+        }
+
+        inputStream.close(); //Close both streams. We don't need them any more.
+        outputStream.close();
+    }
+
+}
